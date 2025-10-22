3ac0e1f239329d3b3f90c1fac74f2c05d4dc958d
Yuge Zhang <scottyugochang@gmail.com>
2025-10-22 12:36:41 +0800

    .

--- examples/tinker/tinker_litellm.py
+++ examples/tinker/tinker_litellm.py
@@ -1,58 +1,141 @@
-from typing import Any, List, TypeGuard
+import uuid
+from typing import Any, List, Literal, TypeGuard
 
 import litellm
+import tinker
 from litellm.llms.custom_llm import CustomLLM
+from litellm.types.utils import ChatCompletionMessageToolCall, ChatCompletionTokenLogprob
+from litellm.types.utils import ChoiceLogprobs as LitellmChoiceLogprobs
 from litellm.types.utils import Choices
 from litellm.types.utils import Message as LitellmMessage
 from litellm.types.utils import ModelResponse
-from tinker_cookbook.renderers import Message, Qwen3Renderer, Renderer
+from litellm.utils import custom_llm_setup
+from tinker.types import ModelInput, SampleResponse, SamplingParams
+from tinker_cookbook.renderers import Message as TinkerMessage
+from tinker_cookbook.renderers import Qwen3Renderer, Renderer
+from tinker_cookbook.renderers import ToolCall as TinkerToolCall
 from transformers.models.auto.tokenization_auto import AutoTokenizer
 
+from agentlightning.logging import configure_logger
 
-def mock_qwen3_token_in_token_out(tokens: list[int]) -> list[int]:
-    return [6, 7, 8, 9, 10]
+service_client = tinker.ServiceClient()
+sampling_client = service_client.create_sampling_client(base_model="Qwen/Qwen3-30B-A3B-Instruct-2507")
+
+logger = configure_logger(name="agentlightning.tinker")
+
+
+def generate_id(prefix: str) -> str:
+    return prefix + str(uuid.uuid4())
 
 
 class TinkerLLM(CustomLLM):
     def __init__(self, renderer: Renderer) -> None:
         self.renderer = renderer
 
-    def _validate_message(self, messages: Any) -> TypeGuard[List[Message]]:
+    def _validate_message(self, messages: Any) -> TypeGuard[List[TinkerMessage]]:
         return True
         # TODO: implement this
         raise NotImplementedError()
 
-    def completion(self, **kwargs: Any) -> litellm.ModelResponse:
+    def _validate_role(self, role: str) -> TypeGuard[Literal["assistant", "user", "system", "tool", "function"]]:
+        return role in ["assistant", "user", "system", "tool", "function"]
+
+    def _parse_tool_call(self, tool_call: TinkerToolCall) -> ChatCompletionMessageToolCall:
+        if set(tool_call.keys()) != {"name", "args"}:
+            logger.warning(f"Found unexpected tool call keys: {tool_call.keys()}")
+        return ChatCompletionMessageToolCall(
+            id=generate_id("tinker-tool-call-"),
+            function={
+                "name": tool_call["name"],
+                "arguments": tool_call["args"],
+            },
+            type="function",
+        )
+
+    def _prepare_model_input(self, **kwargs: Any) -> ModelInput:
         messages = kwargs.pop("messages", None)
         if not self._validate_message(messages):
             raise ValueError("...")
-        inputs = self.renderer.build_generation_prompt(messages)
+        return self.renderer.build_generation_prompt(messages)
 
-        token_out = mock_qwen3_token_in_token_out(inputs)
-        response, parse_success = self.renderer.parse_response(token_out)
+    def _parse_response(self, response: SampleResponse) -> ModelResponse:
+        choices: List[Choices] = []
+        for seq in response.sequences:
+            parsed_response, parse_success = self.renderer.parse_response(seq.tokens)
+            if parse_success:
+                role = parsed_response["role"]
+                if not self._validate_role(role):
+                    raise ValueError(f"Invalid role: {role}")
+                content = parsed_response["content"]
+                tool_calls = parsed_response.get("tool_calls", None)
+                if tool_calls:
+                    tool_calls = [self._parse_tool_call(tool_call) for tool_call in tool_calls]
 
-        return ModelResponse(
-            id="123",
-            choices=[
-                Choices(
-                    message=LitellmMessage(
-                        role=response["role"],
-                        content=response["content"],
+                if seq.logprobs is not None:
+                    logprobs = LitellmChoiceLogprobs(
+                        content=[
+                            ChatCompletionTokenLogprob(
+                                token=str(token),
+                                logprob=logprob,
+                                top_logprobs=[],
+                            )
+                            for token, logprob in zip(seq.tokens, seq.logprobs)
+                        ]
+                    )
+                else:
+                    logprobs = None
+
+                choices.append(
+                    Choices(
+                        message=LitellmMessage(role=role, content=content, tool_calls=tool_calls),
+                        finish_reason=seq.stop_reason,
+                        logprobs=logprobs,
                     )
                 )
-            ],
-        )
+            else:
+                raise ValueError(f"Failed to parse response: {parsed_response}")
+        return ModelResponse(id=generate_id("tinker-sampling-"), choices=choices)
+
+    async def acompletion(self, **kwargs: Any) -> ModelResponse:
+        model_input = self._prepare_model_input(**kwargs)
+        params = SamplingParams(max_tokens=20, temperature=0.0, stop=self.renderer.get_stop_sequences())
+        result = await sampling_client.sample_async(prompt=model_input, sampling_params=params, num_samples=1)
+        return self._parse_response(result)
 
 
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-30B-A3B-Instruct-2507")
-my_custom_llm = TinkerLLM(Qwen3Renderer(tokenizer))
 
-litellm.custom_provider_map = [  # 👈 KEY STEP - REGISTER HANDLER
-    {"provider": "my-custom-llm", "custom_handler": my_custom_llm}
-]
+tinker_llm = TinkerLLM(Qwen3Renderer(tokenizer))
 
-resp = litellm.completion(
-    model="my-custom-llm/my-fake-model",
+litellm.custom_provider_map = [{"provider": "agl-tinker", "custom_handler": tinker_llm}]
+
+custom_llm_setup()
+
+
+from agentlightning import InMemoryLightningStore
+from agentlightning.llm_proxy import LLMProxy
+
+store = InMemoryLightningStore()
+llm_proxy = LLMProxy(
+    port=4000,
+    store=store,
+    model_list=[
+        {
+            "model_name": "Qwen3-30B-A3B-Instruct-2507",
+            "litellm_params": {"model": "agl-tinker/Qwen3-30B-A3B-Instruct-2507"},
+        }
+    ],
+)
+
+llm_proxy.start()
+
+import openai
+
+client = openai.OpenAI(base_url="http://localhost:4000/v1", api_key="dummy")
+response = client.chat.completions.create(
+    model="Qwen3-30B-A3B-Instruct-2507",
     messages=[{"role": "user", "content": "Hello world!"}],
 )
-print(resp)
+print(response)
+
+llm_proxy.stop()
