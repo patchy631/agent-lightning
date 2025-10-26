5679cd21d2141304fb354bc4bfa491d2b719e304
Yuge Zhang <scottyugochang@gmail.com>
2025-10-26 16:31:54 +0800

    update dataset

---
+++ examples/tinker/analyze_test_results.py
@@ -0,0 +1,12 @@
+import os
+
+import pandas as pd
+
+for file in os.listdir("test-results"):
+    if file.endswith(".jsonl"):
+        print(file)
+        df = pd.read_json(os.path.join("test-results", file), lines=True)
+        df = df[df["category"].isin(["animal", "character", "instrument", "nature", "object", "person", "place"])]
+        print(df["correct"].mean())
+        print(df.groupby("category")["correct"].mean())
+        print(df.groupby("category")["correct"].count())
